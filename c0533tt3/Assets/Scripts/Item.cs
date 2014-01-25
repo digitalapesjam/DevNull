8bde4843b2f993c59532d2816d22799c782b3a6e
Francesco Zanitti <fzanitti@gmail.com>
2014-01-25 23:24:29 +0100

    Initial Item script. Added famous and random people photos.

---
+++ c0533tt3/Assets/Scripts/Item.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Item : MonoBehaviour {
+
+	public bool IsEnemy;
+	public GameObject player;
+
+	// Use this for initialization
+	void Start () {
+		Debug.Log ("Item behaviour started - isEnemy? " + IsEnemy + " - player tag: " + player.tag);
+	}
+
+	void OnCollisionEnter2D(Collision2D collider) {
+		Debug.Log ("Entered in a collision " + collider.gameObject.tag);
+		if (collider.gameObject == player) {
+			// Player hit
+			Debug.Log ("Player HIT!");
+			Destroy (this.gameObject);
+
+			if (IsEnemy) {
+				// somehow decrease the player life
+				player.GetComponent<Animator> ().SetTrigger("HurtTrigger");
+			} else {
+				player.GetComponent<Animator> ().SetTrigger("DrinkTrigger");
+			}
+		}
+	}
+
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}

452979163c039e4d463debc6e9bce8bdf25ad9f4
Paolo Burelli <paolo@adm10-59.itu.dk>
2014-01-25 19:49:05 +0100

    Added city parallax

---
+++ c0533tt3/Assets/Background/ProceduralBackground.cs
@@ -0,0 +1,62 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[ExecuteInEditMode]
+public class ProceduralBackground : MonoBehaviour {
+	
+	public Color RainColor = new Color(0.5f,0.5f,1.0f);
+
+	// Use this for initialization
+	void Start () {
+		int height = Screen.height;
+		int width = Screen.width;
+
+		Color transparent = new Color(0,0,0,0);
+		Texture2D map = new Texture2D(width/2,height/2);
+		for (int i=0;i<width;i++)
+			for (int j=0;j<height;j++)
+				map.SetPixel(i,j,transparent);
+
+		for (int i=0;i<width/8;i++)
+			map.SetPixel((int)(Random.value*width),(int)(Random.value*height),RainColor);
+
+		map.Apply();
+
+		renderer.sharedMaterial.SetTexture("_Parallax0",map);
+
+		map = new Texture2D(width/2,height/2);
+		for (int i=0;i<width;i++)
+			for (int j=0;j<height;j++)
+				map.SetPixel(i,j,transparent);
+		
+		for (int i=0;i<width/10;i++)
+			map.SetPixel((int)(Random.value*width),(int)(Random.value*height),RainColor*0.8f);
+		
+		map.Apply();
+
+		renderer.sharedMaterial.SetTexture("_Parallax1",map);
+
+		map = new Texture2D(width,height);
+		for (int i=0;i<width;i++)
+			for (int j=0;j<height;j++)
+				map.SetPixel(i,j,transparent);
+		
+		for (int i=0;i<width/4;i++)
+			map.SetPixel((int)(Random.value*width),(int)(Random.value*height),RainColor*0.8f);
+		
+		map.Apply();
+		
+		renderer.sharedMaterial.SetTexture("_Parallax2",map);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		transform.position = new Vector3(Camera.main.transform.position.x,Camera.main.transform.position.y,0.5f);
+
+		int height = Screen.height;
+		int width = Screen.width;
+		float scaleZ = Camera.main.transform.position.z/-7.5f;
+		float scaleX = scaleZ * 1.01f * ((float)width)/height;
+		transform.localScale = new Vector3(scaleX,1,scaleZ);
+	}
+}
---
+++ c0533tt3/Assets/Maps/Map.cs
@@ -0,0 +1,51 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Map : MonoBehaviour {
+
+	public Sprite city;
+	public Sprite groundBlock;
+	public float LevelLength = 100;
+	public float cityDistance = 0.8f;
+
+	private GameObject farCity;
+	private GameObject floor;
+	private float prevCameraX = 0;
+
+	// Use this for initialization
+	void Start () {
+		int numberOfBlocks = (int)(LevelLength/groundBlock.bounds.size.x);
+		floor = new GameObject("floor");
+		for (int i =0; i<numberOfBlocks; i++){
+			GameObject g = new GameObject("block"+i);
+			g.AddComponent<SpriteRenderer>();
+			g.GetComponent<SpriteRenderer>().sprite = groundBlock;
+			g.AddComponent<PolygonCollider2D>();
+			g.GetComponent<SpriteRenderer>().sortingOrder = 0;
+			g.transform.position = new Vector3(i*groundBlock.bounds.size.x-10,-3,0);
+			g.transform.parent = floor.transform;
+		}
+
+		int numberOfCieties = Mathf.CeilToInt(LevelLength/city.bounds.size.x);
+		farCity = new GameObject("farCity");
+		for (int i =0; i<numberOfCieties; i++){
+			GameObject g = new GameObject("city"+i);
+			g.AddComponent<SpriteRenderer>();
+			g.GetComponent<SpriteRenderer>().sprite = city;
+			g.AddComponent<PolygonCollider2D>();
+			g.GetComponent<SpriteRenderer>().sortingOrder = -1;
+			g.transform.position = new Vector3(i*city.bounds.size.x-10,-0.5f,0);
+			g.transform.parent = farCity.transform;
+		}
+
+		prevCameraX = Camera.main.transform.position.x;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		float cameraOffset = Camera.main.transform.position.x-prevCameraX;
+		prevCameraX = Camera.main.transform.position.x;
+
+		farCity.transform.Translate(cameraOffset*cityDistance,0,0);
+	}
+}

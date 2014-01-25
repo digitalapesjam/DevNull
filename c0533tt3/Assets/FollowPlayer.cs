267c7a6c91943d472d79e0cbdd324071505c70a3
Francesco Zanitti <fzanitti@gmail.com>
2014-01-25 14:11:32 +0100

    Test animazioni e controllo

---
+++ c0533tt3/Assets/FollowPlayer.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FollowPlayer : MonoBehaviour {
+
+	public Vector3 offset;
+
+	private Transform player;
+
+	void Awake () {
+		player = GameObject.FindGameObjectWithTag ("Player").transform;	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		transform.position = player.position + offset;
+	}
+}

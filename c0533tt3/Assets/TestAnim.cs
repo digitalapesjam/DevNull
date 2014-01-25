929e9ad3bfbf73c892c695ef56530cbd88a11df2
Francesco Zanitti <fzanitti@gmail.com>
2014-01-25 04:40:31 +0100

    Added frza scene and sample animations.

---
+++ c0533tt3/Assets/TestAnim.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TestAnim : MonoBehaviour {
+
+	private Animator animator = null;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+
+	void Awake () {
+		this.animator = GetComponent<Animator> ();
+	}
+
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetButtonDown ("Fire1"))
+		{
+//			Debug.Log("Jump!");
+			this.animator.SetBool ("IsJumping", true);
+		} else if (Input.GetButtonUp ("Fire1"))
+		{
+//			Debug.Log("Stop jump!");
+			this.animator.SetBool ("IsJumping", false);
+		}
+	}
+}

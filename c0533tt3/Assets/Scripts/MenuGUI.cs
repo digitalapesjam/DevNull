679625b3fb5a834de7dba14e110432432702134b
Francesco Zanitti <fzanitti@gmail.com>
2014-01-26 11:54:37 +0100

    Added ..urhm.. menu scene. Loads pictures.

---
+++ c0533tt3/Assets/Scripts/MenuGUI.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+
+public class MenuGUI : MonoBehaviour {
+
+	void OnGUI () {
+//		Debug.Log ("Doing GUI");
+		// Make a background box
+		GUI.Box(new Rect(10,10,100,90), "Loader Menu");
+		
+		// Make the first button. If it is pressed, Application.Loadlevel (1) will be executed
+		if(GUI.Button(new Rect(20,40,80,20), "Level 1")) {
+			Debug.Log("Loading Photos");
+			GetComponent<GetPhotos> ().FetchURLs();
+		}
+	}
+
+	public void FBPicsLoaded (List<Texture2D> textures) {
+		GameObject.FindObjectOfType<FbPicturesHolder> ().FbTextures = textures.ToArray();
+		Application.LoadLevel("fbtest");
+	}
+
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
